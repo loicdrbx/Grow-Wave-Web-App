28a8922e991a17490a0ebd4004daa58cf2da5d2f
Loïc Darboux <loic.drbx@gmail.com>
2018-08-16 11:11:48 -0400

    added code to track user auth status.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,26 @@
 import {MDCRipple} from '@material/ripple';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
-console.log("Hello from gwave");
+(function() {
+
+  /** User Authentification Functions */
+
+  var currentUid = null; // Track the UID of the current user
+
+  // Monitor user auth. changes
+  firebase.auth().onAuthStateChanged(function(user) { 
+    // If auth change is a sign-in
+    if (user && user.uid != currentUid) {
+      currentUid = user.uid;     // save userID
+      // handleSignedInUser(user);  // do something
+      console.log("New login from:", user.displayName);
+    } else {
+      // Auth change is a sign-out
+      currentUid = null;                      // reset userID
+      window.location.replace("index.html");  // redirect to landing page
+    }  
+  });
+
+
+
+})();
