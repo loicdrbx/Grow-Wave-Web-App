e0a7cc8525e925dd0e00112ca34a5b96fd355594
Loïc Darboux <loic.drbx@gmail.com>
2018-08-17 17:10:07 -0400

    completed nav bar functionality.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
 /** Import necessary Material Web Components */
 import {MDCRipple} from '@material/ripple';
 import {MDCTopAppBar} from '@material/top-app-bar/index';
+import {MDCDialog} from '@material/dialog';
 
 /** Instantiation of necessary Material Web Components */
-MDCRipple.attachTo(document.querySelector('.foo-button'));
-MDCRipple.attachTo(document.querySelector('.gw-usernamelink'));
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+const dialog = new MDCDialog(document.querySelector('#gw-menu-dialog'));
 
 
 (function() {
@@ -15,7 +15,7 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 
   var currentUid = null; // Track the UID of the current user
 
-  // Monitor user auth. changes
+  // Monitor user auth changes
   firebase.auth().onAuthStateChanged(function(user) { 
     // If auth change is a sign-in
     if (user && user.uid != currentUid) {
@@ -31,9 +31,7 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 
   // Display user's name and photo on toolbar
   function handleSignedInUser(user) {
-
     document.querySelector('.gw-username').textContent = user.displayName;
-
     if (user.photoURL) {
       var photoURL = user.photoURL;
       // Append size to the photo URL for Google hosted images to avoid requesting
@@ -48,8 +46,46 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
     } else {
       document.querySelector('.gw-avatar').style.backgroundImage = "url(./images/avatar.jpg)";
     }
-
-   
-
   }
+  
+  // Monitor sign-out button
+  document.getElementById('sign-out').addEventListener('click', function() {
+    firebase.auth().signOut(); // sign user out
+  });
+
+  // Monitor delete-account button
+  document.getElementById('delete-account').addEventListener('click', function() {
+    deleteAccount();
+  });
+
+  // Delete the user's account
+  function deleteAccount()  {
+    firebase.auth().currentUser.delete().catch(function(error) {
+      if (error.code == 'auth/requires-recent-login') {
+        // The user's credential is too old. They need to sign in again.
+        firebase.auth().signOut().then(function() {
+          // The timeout allows the message to be displayed after the UI has
+          // changed to the signed out state.
+          setTimeout(function() {
+            alert('Please sign in again to delete your account.');
+          }, 1);
+        });
+      }
+    });
+  };
+
+
+  /** Functions for dashboard monitoring/control  */
+
+
+
+  /** Miscellaneous Functioons */
+
+  var menu = document.getElementById('gw-menu');
+
+  menu.addEventListener('click', function (evt) {
+    dialog.lastFocusedTarget = evt.target;
+    dialog.show();
+  });
+
 })();
