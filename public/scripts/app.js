6bf48b87d291d8bd5c0547cf2aafda8f99d305b4
Loic Darboux <loic.drbx@gmail.com>
2018-09-03 11:42:29 -0400

    Added JS to sync user input to database.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -39,7 +39,7 @@ notchedOutlines.forEach(function(notchedOutline) {
     // If auth change is a sign-in
     if (user && user.uid != currentUid) {
       currentUid = user.uid;     // save userID
-      handleSignedInUser(user);  // do something
+      handleSignedInUser(user);  // handle user
       console.log("New login from:", user.displayName);
     } else {
       // Auth change is a sign-out
@@ -93,10 +93,169 @@ notchedOutlines.forEach(function(notchedOutline) {
     });
   };
 
+  /** Dashboard-Control Functions  */
 
-  /** Functions for dashboard monitoring/control  */
+  // Get a reference to the database
+  const db = firebase.database();
+  // Create references to freuquently used paths
+  const deviceRef = db.ref('devices/esp32_1D3438');
 
+  // Sync dashboard to latest data when app opens
+  deviceRef.once('value', function(snapshot) {
 
+    snapshot.forEach(function(childSnapshot) {
+      
+      var key = childSnapshot.key;
+      var data = childSnapshot.val();
+      var elem = document.getElementById(key);
+      var elemClass = elem.className.split(" ");
+      
+      if (elemClass[0] == "updatable-text") {
+        // Special case for time elements
+        if (elemClass[1] == "time") {
+          data = minutesToTime(data);
+        }
+        elem.value = data;
+        // console.log("Updated " + key + " to " + data);
+      }
+      
+      if (elemClass[0] == "updatable-button") {
+        elem.innerText = (data) ? "DISABLE" : "ENABLE";
+        toggleIconInit(elem, data);
+      }
+    });
+  });
+
+  // Monitor updatable buttons for clicks
+  document.querySelectorAll('.updatable-button').forEach(function(button) {
+    button.addEventListener('click', function() {
+      // Toggle button text and icon
+      var val = (button.innerText == "ENABLE") ? true : false;
+      button.innerText = val ? "DISABLE" : "ENABLE";
+      toggleIcon(button);
+      // Commit change to database
+      var newData = new Object();
+      newData[button.id] = val;
+      deviceRef.update(newData)
+      .then(function() {
+        console.log(button.id + "set to " + val);
+      }).catch(function() {
+        console.log("Got an error: ", error);
+      });
+    });
+  });
+
+  // Monitor updatable textfields for changes
+  document.querySelectorAll('.updatable-text').forEach(function(textField) {
+    textField.addEventListener('change', function() {
+      var elem = document.getElementById(textField.id);
+      var elemClass = elem.className.split(" ");
+      var val = this.value
+      var isValid = validateInput(val, textField.id)
+      // Special case for time objects
+      if (elemClass[1] == "time") {
+        console.log(val);
+        val = checkAndConvertTime(val);
+        isValid = val != null;
+      }
+      // Commit change to database if data is valid
+      if (isValid) {
+        var newData = new Object();
+        newData[textField.id] = val;
+        deviceRef.update(newData)
+        .then(function() {
+          console.log(textField.id + " set to " + val);
+        }).catch(function() {
+          console.log("Got an error: ", error);
+        });
+      }
+    });
+  });
+
+  // Toggles the icon associated with a given
+  // HTML element from one state to another
+  function toggleIcon(elem) {
+
+    var iconElem = document.getElementById(elem.id + "-icon")
+    var iconClass = iconElem.className;
+
+    // Special case for grow lights
+    if (iconClass.indexOf("mdi-led") > -1) {
+      iconElem.className = iconClass.indexOf("off") > -1 ?
+                           "card-icon mdi mdi-led-on" :
+                           "card-icon mdi mdi-led-variant-off";
+    } // Every other case (-off is the only change is className)
+    else if (iconClass.indexOf("off") > -1) {
+      iconElem.className = iconClass.slice(0, -4);
+    } else {
+      iconElem.className = iconClass += "-off";
+    }
+  }
+  
+  // Overloaded version of toggleIcon() used when app opens 
+  function toggleIconInit(elem, data) {
+
+    var iconElem = document.getElementById(elem.id + "-icon")
+    var iconClass = iconElem.className;
+    
+    // call toggleIcon() only if icon and button text don't match
+    if ((iconClass.indexOf("off") > -1 && data) ||
+        (iconClass.indexOf("off") < 0 && !data))
+    {
+        toggleIcon(elem);
+    }
+  }
+
+  // Converts number of minutes to a string representing
+  // time in hours and minutes (ex: 45 -> "00:45", 960 -> "16:00")
+  function minutesToTime(minutes) {
+    
+    var hrs = Math.floor(minutes / 60);
+    hrs = ("0" + hrs).slice(-2); // formats 1 hr to 01 hr
+    var mins = minutes % 60;
+    mins = ("0" + mins).slice(-2); // formats 1 min to 01 min
+    if (Number(hrs) > 0) {
+      return hrs + ":" + mins;
+    } else {
+      return "00:" + mins;
+    }
+  }
+
+  // Checks if hh:mm input is valid. Returns a number representing
+  // the total amount of minutes if valid (ex: "16:00" -> 960) and null
+  // otherwise
+  function checkAndConvertTime(timeString) {
+    
+    var minutes = null;
+    // Check that input is a valid hh:mm
+    var isValid = /^([01]\d|2[0-3]):?([0-5]\d)$/.test(timeString);
+    if(isValid) {
+      // Convert hh:mm to an int representing minutes
+      var half = timeString.split(":");
+      minutes = +half[0] * 60 + (+half[1]);
+    }
+    
+    return minutes;
+  }
+  
+  // Returns true if val is a valid input for 
+  // its text-field type and false otherwise
+  function validateInput(val, textType) {
+
+    switch(textType) {
+
+      case "dp1-amount": 
+        return /^[0-9]+$/.test(val) && val >= 25 && val <= 1000;
+      case "dp2-amount": 
+        return /^[0-9]+$/.test(val) && val >= 25 && val <= 1000;
+      case "mp-offTime":
+        return /^[0-9]+$/.test(val) && val >= 5 && val <= 120;
+      case "mp-onTime":
+        return /^[0-9]+$/.test(val) && val >= 5 && val <= 120;
+
+      default: return false;
+    }
+  }
 
   /** Miscellaneous Functioons */
 
